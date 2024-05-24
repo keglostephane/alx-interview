fd3068517049c5586e5450f481893d562151924a
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-24 12:54:04 +0000

    Fix #8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """UTF_8_validation
 
-UTF-8 characters are encoded with a max of 4 bytes.
+UTF-8 code units (characters, glyph) are encoded with a max of 4 bytes.
 the leftmost bits in the start byte indicate the number of bytes
-used to encode the character as following:
+used to encode the character or glyph as following:
 01XXXXXX : 1 byte character
-11XXXXXX : 2 bytes characters
-111XXXXX : 3 bytes characters
-1111XXXX: 4 bytes characters
+11XXXXXX : 2 bytes character
+111XXXXX : 3 bytes character
+1111XXXX: 4 bytes character
 the remaining bytes called continuation bytes : 10XXXXXX
-are used to encode the number.
+are used to encode the character.
 """
 
 
@@ -21,6 +21,7 @@ def validUTF8(data):
     for num in data:
         bits = num & 0xFF   # keep only significant 8 digits
 
+        # check for a new UTF-8 code unit
         if bytes_num == 0:
             # calculate the number of leading 1 in the byte
             while bits & mask:
@@ -29,12 +30,14 @@ def validUTF8(data):
             # detect a ASCII character
             if bytes_num == 0:
                 continue
+            if bytes_num == 1 and num >> 6 == 0b10:
+                continue
             if bytes_num > 4:
                 return False
         else:
             if num >> 6 != 0b10:
                 return False
-        # the number of continuation bytes remaining in data
+        # the number of continuation bytes remaining in a detected code unit
         bytes_num -= 1
 
     return bytes_num == 0
