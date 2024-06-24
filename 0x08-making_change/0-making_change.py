104f9b648756308c7f828e05fe0fbbc8543a1832
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-24 02:53:00 +0000

    Fix #1

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,14 +6,15 @@
 def makeChange(coins, total):
     """Returns the minimum number of coins for a given amount.
     """
-    if not total:
+    if total <= 0:
         return 0
 
-    min_coins = [total + 1] * (total + 1)
-    min_coins[0] = 0
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if amount - coin >= 0:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    return min_coins[total] if min_coins[total] != total + 1 else -1
+    return dp[total] if dp[total] != total + 1 else -1
