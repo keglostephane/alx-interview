52e827422787ffa400f577ff7eb8407cb1061d9c
Keglo Dodzi <stephanekeglo@gmail.com>
2024-07-05 03:51:33 +0000

    Fix #1

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,31 +4,34 @@
 
 
 def isWinner(x, nums):
-    def sieve(n):
-        is_prime = [True] * (n + 1)
-        is_prime[0] = is_prime[1] = False
-        for p in range(2, int(n**0.5) + 1):
-            if is_prime[p]:
-                for multiple in range(p * p, n + 1, p):
-                    is_prime[multiple] = False
-        return [p for p, prime in enumerate(is_prime) if prime]
-
-    def play_game(n):
-        primes = sieve(n)
-        remaining = set(range(1, n + 1))
-        turn = 0
-        for prime in primes:
-            if prime in remaining:
-                remaining -= set(range(prime, n + 1, prime))
-                turn = 1 - turn
-        return turn
-
-    maria_wins = sum(play_game(n) == 1 for n in nums)
-    ben_wins = x - maria_wins
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """Determines the winner of prime game"""
+    if not nums or x < 1:
         return None
+
+    max_num = max(nums)
+
+    is_prime = [True] * (max(max_num + 1, 2))
+    is_prime[0], is_prime[1] = False, False  # 0 and 1 are not primes
+
+    for i in range(2, int(max_num**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_num + 1, i):
+                is_prime[j] = False
+
+    prime_count = 0
+    for i in range(len(is_prime)):
+        if is_prime[i]:
+            prime_count += 1
+        is_prime[i] = prime_count
+
+    maria_wins = 0
+    for n in nums:
+        if is_prime[n] % 2 == 1:
+            maria_wins += 1
+
+    if maria_wins * 2 == len(nums):
+        return None
+    elif maria_wins * 2 > len(nums):
+        return "Maria"
+    else:
+        return "Ben"
